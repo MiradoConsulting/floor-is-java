7de70fcb3f3d8eed3fe7eb1a970391d9b73c0129
Jan CW Kroeze <jcwkroeze@protonmail.com>
2020-03-06 18:00:58 +0100

    random movement

--- src/main/java/FloorIsJava.java
+++ src/main/java/FloorIsJava.java
@@ -1,5 +1,5 @@
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -17,15 +17,19 @@ public class FloorIsJava extends Robot
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setColors(Color.green, Color.green, Color.green, Color.green, Color.green);
+		setAdjustGunForRobotTurn(true);
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			double turnDelta = Math.random() * 30;
+			if (Math.random() > 0.5) {
+				turnRight(turnDelta);
+			} else {
+				turnLeft(turnDelta);
+			}
+		    double moveDelta = Math.random() * 150;
+		    ahead(moveDelta);
 		}
 	}
 
@@ -41,15 +45,13 @@ public class FloorIsJava extends Robot
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
+		turnRight(90);
+		ahead(200);
 	}	
 }
